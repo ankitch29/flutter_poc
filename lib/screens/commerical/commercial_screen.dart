db6d0d1edf966948a0fa8f76df058b0f54e37269
vikshith7 <varsha.vikshith@dreamorbit.com>
2022-05-04 21:57:56 +0530

    added commercial click event

--- lib/screens/commerical/commercial_screen.dart
+++ lib/screens/commerical/commercial_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_poc/screens/chart/bar_chart_3.dart';
+import 'package:flutter_poc/utils/constants.dart';
+import 'package:flutter_poc/utils/ui/responsive.dart';
 import 'package:get/get.dart';
 
 class CommercialScreen extends StatelessWidget {
@@ -7,38 +9,45 @@ class CommercialScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Card(
-          elevation: 24,
-          child: SizedBox(
-            height: 350,
-            width: Get.width,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SizedBox(
-                height: 650,
-                width: Get.width,
-                child: const Card(
-                  elevation: 24,
-                  child: BarChartSample2(),
+    return Scaffold(
+      appBar: Responsive.isMobile(context)
+          ? AppBar(title: const Text("Commercial"),backgroundColor: ColourConstants.accent,)
+          : PreferredSize(
+              child: Container(), preferredSize: const Size(0, 0)),
+      body: Column(
+        children: [
+          Card(
+            elevation: 24,
+            child: SizedBox(
+              height: 350,
+              width: Get.width,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  height: 650,
+                  width: Get.width,
+                  child: const Card(
+                    elevation: 24,
+                    child: BarChartSample2(),
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-        Expanded(child: ListView.builder(
-            itemCount: 100,
-            itemBuilder: (context, index) {
-          return Card(
-            margin: const EdgeInsets.all(8.0),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text("Item - ${index + 1}"),
-            ),
-          );
-        }))
-      ],
+          Expanded(
+              child: ListView.builder(
+                  itemCount: 100,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      margin: const EdgeInsets.all(8.0),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text("Item - ${index + 1}"),
+                      ),
+                    );
+                  }))
+        ],
+      ),
     );
   }
 }
