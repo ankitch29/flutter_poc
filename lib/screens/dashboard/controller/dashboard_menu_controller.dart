ec9561025f0556da19b9473d45f317360185a7da
vikshith7 <varsha.vikshith@dreamorbit.com>
2022-05-05 12:34:33 +0530

    removed un used code.

--- lib/screens/dashboard/controller/dashboard_menu_controller.dart
+++ lib/screens/dashboard/controller/dashboard_menu_controller.dart
@@ -8,43 +8,6 @@ class DashboardMenuController extends GetxController {
   late TabController tabController;
   late TabController tabController2;
 
-  //
-  late PageController pageController;
-  late PageController pageController2;
-
-  @override
-  void onReady() {
-    super.onReady();
-    tabController.addListener(() {
-      pageController.animateToPage(
-        tabController.index,
-        duration: const Duration(milliseconds: 300),
-        curve: Curves.ease,
-      );
-    });
-    tabController2.addListener(() {
-      pageController2.animateToPage(
-        tabController2.index,
-        duration: const Duration(milliseconds: 300),
-        curve: Curves.ease,
-      );
-    });
-    //
-    pageController.addListener(() {
-      tabController.animateTo(
-        pageController.page?.toInt() ?? 0,
-        duration: const Duration(milliseconds: 300),
-        curve: Curves.ease,
-      );
-    });
-    pageController2.addListener(() {
-      tabController2.animateTo(
-        pageController2.page?.toInt() ?? 0,
-        duration: const Duration(milliseconds: 300),
-        curve: Curves.ease,
-      );
-    });
-  }
 
   void navigateToNewCommercial() {
     Get.to(() => const CommercialScreen());
